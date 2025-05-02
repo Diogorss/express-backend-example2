57952feb0960476ec32461725325500e352b2a9d
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-05-02 00:47:05 +0000

    colocando banco de dados no ar

--- api/database/configdb.js
+++ api/database/configdb.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const connect = async () => {
 
-    try{
+    try {
         mongoose.set("strictQuery", true);
         await mongoose.connect(process.env.MONGO_URI, {
             dbName: process.env.MONGO_DB_NAME,
         });
         console.log("MongoDB connected");
     }
-    catch(error){
+    catch (error){
         console.error("MongoDB connection error", error);
         process.exit(1);
     }
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import db from './database/configdb.js';
+import User from './models/User.js' 
 
 dotenv.config();    
 db.connect();
