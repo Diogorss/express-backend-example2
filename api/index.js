2a07a73e10c981bd4538fd7c2044be7bed939486
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-05-05 02:50:50 +0000

    Adicionando bad requests

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import db from './database/configdb.js';
 import User from './models/User.js' 
+import userRoute from './routes/user.route.js';
+import exampleRoute from './routes/example.route.js';
 
 dotenv.config();    
 db.connect();
@@ -10,6 +12,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/user", userRoute);
+app.use("/secureExampleRoute", exampleRoute);
 app.get('/', (req, res) => {
     res.send({message: 'Hello World!'});
 });
