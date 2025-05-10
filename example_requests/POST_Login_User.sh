465fcab9ff7b5b0cc4544bfd95b10124cbd5a4b7
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-05-10 23:37:26 +0000

    corrigindo erro do mongodb

--- example_requests/POST_Login_User.sh
+++ example_requests/POST_Login_User.sh
@@ -2,6 +2,6 @@ curl --request POST \
   --url 'http://localhost:3000/user/login' \
   --header 'Content-Type: application/json' \
   --data '{
-    "username": "newuser",
+    "username": "newuser4",
     "password": "securepassword123"
     }'
