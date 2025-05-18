ce4bbc20ca184085a5e72f60b008f7cd538efe5d
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-05-18 17:10:28 +0000

    corrigindo logs vercel

--- example_requests/POST_Register_User.sh
+++ example_requests/POST_Register_User.sh
@@ -1,3 +1,3 @@
 curl -X POST http://localhost:3000/api/users/register \
   -H "Content-Type: application/json" \
-  -d '{"username": "novousuario2", "password": "senhasegura123"}' 
+  -d '{"username": "novousuario2", "password": "aa"}' 
